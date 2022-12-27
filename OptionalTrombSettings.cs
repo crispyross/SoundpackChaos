d35e986994d4a0ae7d892ba1abb47928ade7a9f9
CrispyRoss <crispyross1@gmail.com>
2022-12-27 15:11:09 -0600

    Add project files.

---
+++ OptionalTrombSettings.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace SoundpackChaos;
+
+public static class OptionalTrombSettings
+{
+    public static object? GetConfigPage(string pageName)
+    {
+        try
+        { 
+            Type? trombConfig = null;
+            trombConfig = Type.GetType("TrombSettings.TrombConfig, TrombSettings");
+            if (trombConfig == null)
+            {
+                Plugin.Logger.LogInfo("TrombSettings not found.");
+                return null;
+            }
+
+            var trombSettingsInstance = trombConfig.GetField("TrombSettings").GetValue(null);
+            var indexerMethod = trombSettingsInstance.GetType().GetIndexer(typeof(string));
+            var settingsPage = indexerMethod.GetGetMethod().Invoke(trombSettingsInstance, new object[] { pageName });
+            return settingsPage;
+        }
+        catch (Exception e)
+        {
+            Plugin.Logger.LogWarning("Exception trying to get config page. Reporting TrombSettings as not found.");
+            Plugin.Logger.LogWarning(e.Message);
+            Plugin.Logger.LogWarning(e.StackTrace);
+            return null;
+        }
+    }
+
+    public static void AddSlider(object page, float min, float max, float increment, bool integerOnly, ConfigEntryBase entry)
+    {
+        try
+        {
+            Type? clazz = Type.GetType("TrombSettings.StepSliderConfig, TrombSettings");
+            if (clazz == null)
+                return;
+            var ctor = clazz.GetConstructor(new Type[] { typeof(float), typeof(float), typeof(float), typeof(bool), typeof(ConfigEntryBase) });
+            var slider = ctor?.Invoke(new object[] { min, max, increment, integerOnly, entry });
+
+            if (slider != null)
+            {
+                // Find "public new void Add(BaseConfig configEntry)"
+                Type? baseConfigClass = Type.GetType("TrombSettings.BaseConfig, TrombSettings");
+                var addMethod = page.GetType().GetMethod("Add", new Type[] { baseConfigClass });
+                addMethod.Invoke(page, new object[] { slider });
+            }
+            else
+                Plugin.Logger.LogError("Couldn't create slider!");
+        }
+        catch (Exception e)
+        {
+            Plugin.Logger.LogError("Exception trying to create slider. Reporting TrombSettings as not found.");
+            Plugin.Logger.LogError(e.Message);
+            Plugin.Logger.LogError(e.StackTrace);
+        }
+
+    }
+
+
+    public static void Add(object page, ConfigEntryBase entry)
+    {
+        var addFn = page.GetType().GetMethod("Add", new Type[] { typeof(ConfigEntryBase) });
+        addFn.Invoke(page, new object[] { entry });
+    }
+}
+
+
+internal static class TypeExtensions
+{
+    // From https://stackoverflow.com/a/55457150
+
+    public static PropertyInfo GetIndexer(this Type type, params Type[] arguments) => type.GetProperties().First(x => x.GetIndexParameters().Select(y => y.ParameterType).SequenceEqual(arguments));
+}
---
+++ Plugin.cs
@@ -0,0 +1,69 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using SoundpackLoader;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace SoundpackChaos;
+
+
+public record MetaData
+{
+    public const string PLUGIN_NAME = "SoundpackChaos";
+    public const string PLUGIN_GUID = "org.crispykevin.soundpackchaos";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
+
+[BepInPlugin(MetaData.PLUGIN_GUID, MetaData.PLUGIN_NAME, MetaData.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static new ManualLogSource Logger = null!;
+    public static ConfigEntry<bool> Enabled = null!;
+    public static ConfigEntry<bool> IncludeVanilla = null!;
+
+    public Plugin()
+    {
+        Logger = base.Logger;
+    }
+
+    private void Awake()
+    {
+        new Harmony(MetaData.PLUGIN_GUID).PatchAll();
+
+        Enabled = Config.Bind("General", "Enabled", true);
+        IncludeVanilla = Config.Bind("General", "Include Vanilla Soundpacks", true);
+
+        var page = OptionalTrombSettings.GetConfigPage("SoundpackChaos");
+        if (page != null)
+        {
+            OptionalTrombSettings.Add(page, Enabled);
+            OptionalTrombSettings.Add(page, IncludeVanilla);
+        }
+        
+        Logger.LogInfo($"Plugin {MetaData.PLUGIN_GUID} v{MetaData.PLUGIN_VERSION} is loaded!");
+    }
+}
+
+[HarmonyPatch(typeof(GameController))]
+class GameControllerPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(GameController.playNote))]
+    static void BeforeNote(GameController __instance)
+    {
+        var self = __instance;
+        if (Plugin.Enabled.Value)
+        {
+            var soundpacks = Soundpack.GetAll().AsEnumerable();
+            if (!Plugin.IncludeVanilla.Value)
+                soundpacks = soundpacks.Where(s => !s.IsVanilla);
+            self.ChangeSoundpack(soundpacks.GetRandom());
+        }
+    }
+
+
+}
